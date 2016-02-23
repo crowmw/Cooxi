ded2d34c977518180aec53458d58421edeb568ba
Mateusz Woźniak <crow.mw@gmail.com>
2016-02-23 21:14:26 +0100

    main layout added

--- Cooxi/App_Start/BundleConfig.cs
+++ Cooxi/App_Start/BundleConfig.cs
@@ -24,7 +24,11 @@ namespace IdentitySample
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/UserBar.css",
+                      "~/Content/SearchBar.css",
+                      "~/Content/NavigationBar.css",
+                      "~/Content/SideBar.css"));
         }
     }
 }
